2bb2f71ab870d1b6900587bc12f5701112e2859a
Battousai <crea18@mail.ru>
2023-06-17 11:44:08 +0500

    Дз к первому семинару

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿//Задача2
+/*System.Console.WriteLine("Введите числа a и b");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if(a>b)
+{
+    System.Console.WriteLine(a);
+
+}
+else
+{
+    System.Console.WriteLine(b);
+}*/
+
+
+//Задача 4
+/*System.Console.WriteLine("Введите числа a,b,с");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+if(a > b )
+{
+    if(a > c)
+    {
+         System.Console.WriteLine(a);
+    }
+    else
+    {
+        System.Console.WriteLine(c);
+    }
+  
+}
+else if(b > c)
+{
+    System.Console.WriteLine(b);
+}
+else
+{
+    System.Console.WriteLine(c);
+}*/
+
+
+//Задача 6
+/*System.Console.WriteLine("Введите число a");
+int a = Convert.ToInt32(Console.ReadLine());
+int num = a % 2;
+if(num == 0)
+{
+    System.Console.WriteLine("Введенное число четное");
+}
+else
+{
+    System.Console.WriteLine("Введенное число нечетное");
+}*/
+
+//Задача 8
+/*System.Console.WriteLine("Введите диапозон чисел");
+int a = Convert.ToInt32(Console.ReadLine());
+for(int i = 1; i < a; i++)
+if(i % 2 == 0)
+{
+    System.Console.Write(i + " " );
+}*/
