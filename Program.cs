ee0ee334a914666f447b0bc1ae4d7b3bd3d705e8
Battousai <crea18@mail.ru>
2023-06-18 14:24:05 +0500

    Домашнее задание ко второму семинару

--- Program.cs
+++ Program.cs
@@ -61,3 +61,57 @@ if(i % 2 == 0)
 {
     System.Console.Write(i + " " );
 }*/
+
+
+//Задача 10
+/*System.Console.WriteLine("Введите трехзначное число");
+int a= Convert.ToInt32(Console.ReadLine());
+int Length = a.ToString().Length;
+if (Length == 3) 
+{
+	int result = ( a/10 ) % 10;
+	Console.WriteLine("Второе число " + result);
+} 
+else 
+{
+	Console.WriteLine("Не трехзначное число");
+}*/
+
+
+//Задача 15
+/*System.Console.WriteLine("Введите день недели");
+int Day_of_Week = Convert.ToInt32(Console.ReadLine());
+if (Day_of_Week >= 1 && Day_of_Week <=7)
+{
+    if (Day_of_Week >= 6) 
+    {
+		System.Console.WriteLine("Этот день выходной");
+	} 
+    else 
+    {
+		System.Console.WriteLine("Рабочий день недели");
+	}
+} 
+else 
+{
+	System.Console.WriteLine("Введите точное число дня недели");
+}*/
+
+
+//Задача 13
+/*System.Console.WriteLine("Введите число");
+int Value2 = Convert.ToInt32(Console.ReadLine());
+int Length2 = Value2.ToString().Length;
+if (Length2 >= 3) 
+{
+	while (Value2 > 999)
+	{
+		Value2 = Value2 / 10;
+	}
+	int result = Value2 % 10;
+	Console.WriteLine("Третье число " + result);
+} 
+else 
+{
+	Console.WriteLine("У данного числа нет третьей цифры");
+}*/
